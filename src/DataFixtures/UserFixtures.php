3fddd8125038f01131e11fc808e5bc10a722d185
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-04-09 22:24:36 +0200

    add user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture implements FixtureGroupInterface
+{
+    /**
+     * @return array<string, string>
+     */
+    public static function getGroups(): array
+    {
+        return ['user', 'all'];
+    }
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $items = [
+            [
+                'email'    => 'admin@admin.local',
+                'password' => 'admin',
+                'roles'    => ['ROLE_ADMIN'],
+            ],
+        ];
+
+        foreach ($items as $item) {
+            $user = new User();
+            $user
+                ->setEmail($item['email'])
+                ->setPassword($this->passwordHasher->hashPassword(
+                    $user,
+                    $item['password'])
+                )
+                ->setRoles($item['roles'])
+            ;
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
