ca7e9c31377826e1b7548737103ce7cce05d02ee
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-04-15 06:55:31 +0200

    add health check

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthCheckController extends AbstractController
+{
+    #[Route('/health', name: 'app_health_check', methods: [Request::METHOD_GET])]
+    public function check(string $appVersion): JsonResponse
+    {
+        $message = [
+            'version'   => $appVersion,
+            'timestamp' => new \DateTime('now'),
+        ];
+
+        return $this->json($message);
+    }
+}
