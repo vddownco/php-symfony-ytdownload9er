d3d32f418dcd249c862a8812e5add8b24cd78eb6
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-05-11 11:14:11 +0200

    move security controller to src

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller\Ui;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
